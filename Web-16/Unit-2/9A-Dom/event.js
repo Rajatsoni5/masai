754552406b78eb3631f83100130da3398f0c01cb
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-03-08 23:44:55 +0530

    ok

---
+++ Web-16/Unit-2/9A-Dom/event.js
@@ -0,0 +1,21 @@
+// function myFunction() {
+//     console.log("Iam clicked")
+// }
+
+// new method
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+    console.log("java")
+})
+
+
+// event argument
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+    console.log("java")
+})
+
