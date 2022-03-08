848bd4ca36f44313be38c49cc8bb70301bdd2fd6
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-03-08 23:14:55 +0530

    push it

--- Web-16/Unit-2/9A-Dom/index.js
+++ Web-16/Unit-2/9A-Dom/index.js
@@ -7,3 +7,42 @@
 // var heading = document.getElementById("heading");
 
 // heading.innerHTML = "Java";
+
+// Traversing Nodes
+
+// var body = document.querySelector("body")
+// console.log(body.parentElement)
+
+
+// creating element
+
+// Elemnet
+// var h1 = document.createElement('h1')
+// console.log(h1)
+
+// text add
+// h1.className="heading"
+// h1.id="heading1"
+// h1.setAttribute('title',"iam heading")
+
+// var headingText = document.createTextNode("This is my heading");
+
+// h1.appendChild(headingText);
+
+// adding to body
+// var body=document.querySelector("body");
+// var p=document.querySelector("p");
+
+// body.insertBefore(h1,p)
+
+
+// Removing 
+
+// find parent
+// var body =document.querySelector("body");
+// var button = document.querySelector("button");
+
+// write remove thing here
+// body.removeChild(button)
+
+
