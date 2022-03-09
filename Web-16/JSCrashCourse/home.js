d165f8796ae169567984da11c2eb75d6a3eecba5
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-03-09 20:16:13 +0530

    ok

---
+++ Web-16/JSCrashCourse/home.js
@@ -0,0 +1,106 @@
+// *****************************************variables
+
+// var b = 'smoothies'
+// console.log(b);
+
+// cousin of alert
+// var age= prompt('what is your age?');
+
+// get the value and print
+// document.getElementById('sometext').innerHTML = age;
+
+// ************************************Numbers in js
+
+// var num1 = 7;
+// can make chnages by below (cuz code work up to down)
+// Here's the new num1 value (Increment)
+// num1 = num1 + 2;
+//you can write "num1++"" as well 
+//(you can decrement by Num1--)can divide /, multiple * and remander %
+// console.log(num1*2)
+
+// Increment/decrement by any number
+// num1 += 20
+// console.log(num1)
+
+
+// *************************************Functions
+// 1* create the function
+// 2* call the function
+
+// function fun() {
+//     alert("hello")
+// }
+
+// call the function to see activity of above written function
+// fun();
+// done you called it and your fuction is working
+
+
+// example of new funtion 
+
+
+
+// function greetings(yourName){
+    
+    // var result = 'Hello ' + yourName;  // string concatenation
+
+    // choose any from below to function to see the activity
+
+    // document.getElementById('sometext').innerHTML = result;
+    // console.log(result);
+// };
+
+// var name = prompt('what is your name?')
+// greetings(name);
+
+
+
+//**************************How do arguments work in functions?
+// how do we add 2 numbers together in a functions?
+
+// function sum(num1,num2) {
+
+//     var result = num1 + num2;
+
+//     console.log(result);
+// }
+
+// Put the value while calling function so that values will assign accordingly.
+// choose any option below to see the result respectively.
+// sum("Hello ", "rajat");
+// sum(2,3)
+
+
+// *************************while and for loops in js
+// while loops
+
+
+// var num = 0;
+
+// while(num < 100){
+//     num += 10;
+//     console.log(num);
+// }
+
+// for loop
+
+// for(let num =0; num<=100; num++){
+//     console.log(num)
+// }
+
+
+//*********************************Data types in js
+
+let yourage =20; // Number
+let yourname = 'Rajat'; // string
+let name = {first:'Rajat', last:'soni'} // object
+let truth = false; //Boolean
+let arr = ['one','two','three']; //array
+let random; //undefind
+let nothing = null; //value null
+
+
+// *************************** strings(common method)
+
+let fruit = 'Apple';
