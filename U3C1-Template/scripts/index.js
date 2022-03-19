1146224a3614c8ea2aaa7aa707fb71ea9a28b549
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-03-19 23:35:11 +0530

    ok

--- U3C1-Template/scripts/index.js
+++ U3C1-Template/scripts/index.js
@@ -1,11 +1,24 @@
 //store the products array in localstorage as "products"
-
 document.getElementById("submit").addEventListener("click",function(){
-    var result = {
-        name: document.getElementById("name").value,
-        price: document.getElementById("price").value,
-        type: document.getElementById("type").value,
-        image: document.getElementById("image").value,
+    
+    var get_data =  JSON.parse(localStorage.getItem("all_entry"))
+    if(get_data == null){
+        get_data = []
     }
-    localStorage.setItem("product",result)
+    var result = {
+
+        "name": document.getElementById("name").value,
+        "price": document.getElementById("price").value,
+        "type": document.getElementById("type").value,
+        "image": document.getElementById("image").value,
+    }
+    localStorage.setItem("entry", JSON.stringify(result))
+    console.log(get_data)
+    get_data.push(result)
+
+    localStorage.setItem("all_entry", JSON.stringify(get_data))
 });
+
+function show() {
+    window.location.href = "./inventory.html"
+}
---
+++ U3C1-Template/scripts/inventory.js
@@ -0,0 +1,61 @@
+var stock = JSON.parse(localStorage.getItem("all_entry")) 
+
+var product_details = document.getElementById("products_data");
+
+function add() {
+    window.location.href = "./index.html"
+}
+
+console.log(stock);
+
+for(var j=0; j<stock.length; j++){
+    const name1 = document.createElement("h4")
+    name1.innerText = stock[j].name
+
+    const image = document.createElement("img")
+    image.src = stock[j].image
+
+    const type = document.createElement("p")
+    type.innerText = stock[j].type
+
+    const price = document.createElement("p")
+    price.innerText = stock[j].price
+
+    const button = document.createElement("button")
+    button.innerText = "Remove"
+    button.className = "remove_data"
+
+    const div = document.createElement("div")
+    div.className = "product"
+
+    button.addEventListener("click", function(event){
+        let button = event.target
+        button.parentNode.parentNode.removeChild(button.parentNode)
+        
+        // var local_data = localStorage.getItem("all_entry")
+
+        // stock.splice(j,1)
+        // localStorage.setItem("all_entry", JSON.stringify(stock))
+
+
+    })
+    div.append(name1,type,price,button)
+    product_details.append(div)
+
+
+}
+
+console.log(product_details)
+// const remove_data1 = document.getElementsByClassName("remove_data")
+// console.log(remove_data1)
+
+// for(var i=0; i<remove_data1.length; i++){
+//     remove_data1[i].addEventListner("click", function(){
+//         localStorage.removeItem("product")
+//         console.log("removed")
+//     })
+    
+// }
+
+
+
