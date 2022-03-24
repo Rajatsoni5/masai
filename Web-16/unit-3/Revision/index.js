cddd14fcab4b0466fad5d1e463edf5e3ada102cb
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-03-24 22:32:55 +0530

    ok

--- Web-16/unit-3/Revision/index.js
+++ Web-16/unit-3/Revision/index.js
@@ -1,10 +1,32 @@
 // clouser - function under function
 
-function A () {
-    let x =1;
-    function B() {
-        let y  =2;
-        return y;
-    }
-    return x;
+// function A () {
+//     let x =1;
+//     function B() {
+//         let y  =2;
+//         return y;
+//     }
+//     return x;
+// }
+
+
+// fetch
+
+// fetch("write api")
+// .then((res)=> res.json())
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// });
+
+// IMport/Export
+
+const app = document.getElementById("app")
+
+function sum(a, b){
+    return a+b
 }
+
+app.innerText = sum(3,5)
