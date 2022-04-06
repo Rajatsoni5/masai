4e917e33d07c886a35ca4de51a95d2a1deae89e7
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-04-06 19:07:34 +0530

    ok

---
+++ Web-16/Unit-3A/sprint1/Howjswork/Assign/Minijs/index.js
@@ -0,0 +1,3 @@
+//This information should be stored in 4 different objects. All of this objects should be stored in localstorage in advance. Their localstorage key should be their respective names.  For e.g Scope data will be stored in LocalStorage with key as "scope". ( Note: no capital letter in key )
+
+//Information should be retrieved from localstorage only, avoid hard coding data.
