696d3c69f633f7195fc800a5a40693dbc06335a9
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-04-13 23:36:39 +0530

    ok

--- Web-16/Unit-3A/sprint2/fetch1/assign/ombd1/index.js
+++ Web-16/Unit-3A/sprint2/fetch1/assign/ombd1/index.js
@@ -1,19 +1,34 @@
-let res;
 
-async function movie() {
+async function searchHit() {
 
+    let moviename = document.getElementById("search").value;
+    let print = document.getElementById("print");
+
+    console.log("movie", moviename) 
     try {
         
-        let data = await fetch(`http://www.omdbapi.com/?apikey=[2aea2a57]&t`)
-
-        res = await (data.json())
+        let data = await fetch(`https://www.omdbapi.com/?t=${moviename}&apikey=2aea2a57
+        `)
         
-        console.log("res:",res)
+        let res = await data.json()
+        
+        console.log("data",res)
+
+        if(res.error) {
+            print.innerHTML = res.Error
+        } else {
+            let h3 = document.createElement("h3");
+            h3.innerText = res.Title;
+
+            print.appendChild(h3)
+        }
+
 
 
     } catch (error) {
-        console.log("error", error)
+        print.innerText = error.message
     }
+    moviename.innerText = ""
 }
 
-movie();
+// movie();
