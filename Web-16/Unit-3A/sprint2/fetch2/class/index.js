e0e4e058121481c0c99ca33b80db227247d58f72
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-04-14 23:42:16 +0530

    updated whether app

--- Web-16/Unit-3A/sprint2/fetch2/class/index.js
+++ Web-16/Unit-3A/sprint2/fetch2/class/index.js
@@ -1,98 +1,88 @@
-// src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
- 
- async function Getit(){
-
-//    try {
+function getData() {
     let city = document.getElementById("city").value;
-
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2cc6f2187caea11b4c880e14cc75c11`)
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(res){
-        append(res)
-        console.log(res)
-    })
-    .catch(function (err){
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2cc6f2187caea11b4c880e14cc75c11
+    `;
+  
+    fetch(url)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (res) {
+        append(res);
+        console.log(res);
+      })
+      .catch(function (err) {
         console.log(err);
-    });
-
-//     let res = await data.json();
-//     console.log(res)
-//     append(res)
-   
-
-//    } catch (error) {
-//        console.log(error)
-//    }
-
-
-}
-
-function getdata2(lat,lon){
-    let city = document.getElementById("city").value;
-
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f2cc6f2187caea11b4c880e14cc75c11`)
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(res){
-        append(res)
-        console.log(res)
-    })
-    .catch(function (err){
-        console.log(err);
-    });
-
-
-}
-
-
-function append(data){
-
-    let container = document.createElement("container");
-    container.className = "container"
+      });
+  }
+  
+//   function getDataLocation(lat, lon) {
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f2cc6f2187caea11b4c880e14cc75c11
+//     `;
+  
+//     fetch(url)
+//       .then(function (res) {
+//         return res.json();
+//       })
+//       .then(function (res) {
+//         append(res);
+//         console.log(res);
+//       })
+//       .catch(function (err) {
+//         console.log(err);
+//       });
+//   }
+  
+  function append(data) {
+    let container = document.getElementById("container");
+    let map = document.getElementById("gmap_canvas");
     container.innerHTML = null;
-
-    let map = document.getElementById("gmap_canvas")
-
-
-    let city = document.createElement("h4")
+  
+    let city = document.createElement("h2");
     city.className = "city"
-    city.innerText = `City: ${data.city}`
+    city.innerText = `City: ${data.name}`;
+  
+    let min = document.createElement("p");
+    min.innerText = `Min temp: ${data.main.temp_min}`;
+    min.className = "text"
+  
+    let max = document.createElement("p");
+    max.innerText = `Max temp: ${data.main.temp_max}`;
+    max.className = "text"
+  
+    let clouds = document.createElement("p");
+    clouds.innerText = `Current Temp: ${data.clouds.all}`;
+    clouds.className = "text"
 
-    let min = document.createElement("p")
-    min.innerText = `Min_temp: ${data.main.temp_min}`
-    
-    let max = document.createElement("p")
-    min.innerText = data.main.temp_max;
+    let sunrise = document.createElement("p");
+    sunrise.innerText = `Sunrise: ${data.sys.sunrise}`;
+    sunrise.className = "text"
 
-    let humidity = document.createElement("p")
-    humidity.innerText = data.main.humidity;
-
-    let current = document.createElement("p")
-    current.innerText = data.main.temp;
-
-    container.append(city, min, max, humidity, current)
-    map.src = `https://maps.google.com/maps?q=${data.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`
-}
-
-
-function getweather(){
-
-    navigator.geolocation.getCurrentPosition(success);
-
-    function success(pos){
-        var crd = pos.coords;
-
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`More or less ${crd.accuracy} meters.`);
-
-        getdata2(crd.latitude,crd.longitude)
-    }
- 
-}
-
+    let sunset = document.createElement("p");
+    sunset.innerText = `Sunset: ${data.sys.sunset}`;
+    sunset.className = "text"
+  
+    let wind = document.createElement("p");
+    wind.innerText = `Wind: ${data.wind.speed}`;
+    wind.className = "text"
+  
+    container.append(city,sunrise,sunset, min, max, clouds, wind);
+    map.src = `https://maps.google.com/maps?q=${data.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+  }
+  
+//   function getWeather() {
+//     navigator.geolocation.getCurrentPosition(success);
+  
+//     function success(position) {
+//       let crd = position.coords;
+  
+//       console.log("Your current position is:");
+//       console.log(`Latitude : ${crd.latitude}`);
+//       console.log(`Longitude: ${crd.longitude}`);
+//       console.log(`More or less ${crd.accuracy} meters.`);
+  
+//       getDataLocation(crd.latitude, crd.longitude);
+//     }
+//   }
+  
