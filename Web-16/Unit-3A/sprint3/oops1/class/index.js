a3695915544096ae6abeee8be0edcc3a0ca6d3a9
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-04-22 12:37:52 +0530

    ok

---
+++ Web-16/Unit-3A/sprint3/oops1/class/index.js
@@ -0,0 +1,15 @@
+// ES6 class - create object
+
+class Car {
+    constructor(n,b){
+        this.name = n;
+        this.brand = b;
+    }
+}
+
+let C1 = new Car("swift", "maruti");
+console.log(C1);
+
+//Object Oriented Programming
+
+// 1.
