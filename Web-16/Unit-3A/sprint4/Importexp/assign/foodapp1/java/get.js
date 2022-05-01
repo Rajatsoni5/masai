881acf30398724d0cfced84e1d25602917667615
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-05-01 23:13:43 +0530

    ok

--- Web-16/Unit-3A/sprint4/Importexp/assign/foodapp1/java/get.js
+++ Web-16/Unit-3A/sprint4/Importexp/assign/foodapp1/java/get.js
@@ -2,3 +2,7 @@ import { navbar } from "../components/navbar.js";
 
 let nav = document.getElementById("navbar");
 nav.innerHTML = navbar();
+
+let res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
+let data = await res.json();
+console.log('data:', data)
