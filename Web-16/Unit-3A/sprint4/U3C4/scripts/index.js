439b71808a53457d30f086ee395863b241a23ffb
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-05-02 18:46:32 +0530

    ok

---
+++ Web-16/Unit-3A/sprint4/U3C4/scripts/index.js
@@ -0,0 +1,6 @@
+// Ude Import export (MANDATORY)
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+async function show() {
+
+    let res = await fetch("")
+}
---
+++ Web-16/Unit-3A/sprint4/U3C4/scripts/search.js
@@ -0,0 +1,2 @@
+// Ude Import export (MANDATORY)
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
